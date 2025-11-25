6baa8cb08eb5786985a00580dca5899609435bb9
vladislav <ubuhlayeu@gmail.com>
2025-11-25 12:52:23 +0300

    Add default 8080 API port

--- src/lib/rest/constants/ApiConstants.ts
+++ src/lib/rest/constants/ApiConstants.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || `http://localhost:${window.location.port}`
+export const API_URL = import.meta.env.VITE_API_URL || `http://localhost:${window.location.port || 8080}`
